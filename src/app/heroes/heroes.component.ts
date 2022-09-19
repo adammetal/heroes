d5d4b90c3248ad75e3240f9dfead9f270f8d3dcd
adam.metal <adam.metal@codecool.com>
2022-09-19 18:22:08 +0200

    start implement the log service and log ui

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,23 +8,17 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Chandra',
-    cost: 6,
-    loyalty: 6,
-    first: 1,
-    sec: -4,
-    ult: -7
-  };
-
-  heroes = HEROES;
+  heroes?: Hero[];
 
   selectedHero?: Hero;
 
-  constructor() {}
+  constructor(private heroesService: HeroService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.heroesService.getHeroes().subscribe((heroes) => {
+      this.heroes = heroes;
+    });
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
---
+++ src/app/log.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Stack } from './stack';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogService {
+  private messages: Stack<string> = new Stack();
+
+  constructor() { }
+
+  addMessage(msg: string) {
+    this.messages.push(msg);
+  }
+
+  popMessage() {
+    this.messages.pop();
+  }
+}
